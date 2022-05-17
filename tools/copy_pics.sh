e442b930b077923ccb241fa8f2c8fe6a562bdf67
Yaroslav Halchenko <debian@onerussian.com>
2022-05-17 10:18:31 -0400

    gulp is misbehaving

--- tools/copy_pics.sh
+++ tools/copy_pics.sh
@@ -6,8 +6,9 @@ sed -n -e '/<img/s,.*data-src="\([^"]*\)".*,\1,gp' index.html \
         echo -n "$p missing"
         for d in /home/yoh/proj/repronim/artwork/talks/webinar-2020-reprocomp \
 					 /home/yoh/proj/datalad/datalad-course \
-					 home/yoh/proj/experiments/nuisance/presentations/2020-NNL \
+					 /home/yoh/proj/experiments/nuisance/presentations/2020-NNL \
 					 /home/yoh/proj/dandi/artwork \
+					 /home/yoh/proj/pymvpa/papers \
 				 ; do
             c="$d/$p"
             if [ -e "$c" ]; then
