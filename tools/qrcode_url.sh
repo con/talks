2aa587ca85edbd9bfdfc3c4ea28da7b6f242d0bc
Yaroslav Halchenko <debian@onerussian.com>
2025-10-21 11:21:19 -0400

    Make script provide original command in commit msg so I do not forget

--- tools/qrcode_url.sh
+++ tools/qrcode_url.sh
@@ -10,6 +10,9 @@ out="${url##*/}"
 out="pics/${out%%.*}-qrcode.png"
 
 
-datalad run -m "Produce QR code for $url" --output "$out" --explicit qrencode -s 50 -d 300 -l H -o "$out" "$url"
+datalad run -m "Produce QR code for $url.
+
+Original command was: $@" \
+       --output "$out" --explicit qrencode -s 50 -d 300 -l H -o "$out" "$url"
 
 echo "Done. Output file: $out"
